c66fac1efba2cc7f11369544fc72bdfc98f2f59e
MonserratRivas <catalinarivas.iccp@gmail.com>
2022-08-19 10:50:10 -0400

    subiendo predicciones

---
+++ PredecirCodigo.js/Predicción2.js
@@ -0,0 +1,31 @@
+function muestraInformaciónContacto() {
+    var auntContactInfo = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo);
+}
+        //console.log; imrpimira (Paula, Smith, Calle Principalm 1234, St. Louis, MO, 12345)
+
+       //variable                                Values
+ //var auntContectInfo        Paula, Smith, Calle Principal 1234,  St. Louis, MO, 12345
+
+
+
+function muestraListaDeCompras() {
+    var produce = ["manzanas", "naranjas"];
+    var frozen = ["brócoli", "helado"];
+    frozen.push("croqueta de papa");
+    console.log(frozen);
+}
+        //console.log; mostrara (brocóli, helado, croqueta de papa)
+
+    //     Varieble                     Values
+//      var produce                manzana,naranjas
+//      var frozen                 [brócoli, helado]  >   [brócoli, helado, croqueta de papa]
+
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zoro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+        //console.log; mostrara (Bambi, Beetlejuice, "Toy Story", Zoro)
+
+  //      Variable                     Values
+//    var movieLibrary         Bambi, E.T, Toy Story  >  Bambi, E.T, Toy Story, Zoro  >  Bambi, Beetlejuice, Toy Story, Zoro
